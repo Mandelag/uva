7319d8bb9ae924f8c1263b0a79ab6f4b521aff0c
keenan.gebze <keenan.gebze@gmail.com>
2021-07-17 21:22:03 +0700

    close but no cigar! for cd problem

--- chapter3/complete-search/recursive-easy/cd/main.cpp
+++ chapter3/complete-search/recursive-easy/cd/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
-void print_solution(int n, int tracks, std::vector<int> ts);
-void minimizeUnused(int n, int tracks, std::vector<int> ts);
-int get_sum(std::vector<int> tracks, int start_idx);
+bool print_solution(std::stack<int>& solution, int& sum, std::vector<int> track, int idx, int n);
+void printStack(std::stack<int> some_stack);
+int sumStack(std::stack<int> some_stack);
 
 int main() {
 	int counter{0};
@@ -16,38 +17,79 @@ int main() {
 		if (stream.eof()) {
 			break;	
 		}
-		std::vector<int> t(track);
 		
+		std::vector<int> t(track);
+		std::stack<int> solution;
+
 		for (int i{0}; i < track; i++) {
 			int duration{0};
 	
 			std::cin >> duration;
 			t.at(i) = duration;
 		}
-
-		get_sum(t, 0);
+	
+		int sum{0};
+		print_solution(solution, sum, t, 0, n);
+		// std::cout << "Final solution: ";
+		
+		printStack(solution);
+		std::cout << "sum:" << sumStack(solution) << "\n";
 
 		// print_solution(n, track, t);
 		counter++;	
 	}	
 }
 
-void print_solution(int n, int tracks, std::vector<int> ts) {
-	std::vector<int> result(ts.size());
+bool print_solution(std::stack<int>& solution, int& sum, std::vector<int> track, int idx, int n) { 
+	// count current sum from solution
+	if (idx >= track.size()) {
+		return true;
+	}
 
+	solution.push(track.at(idx));
+	
+	if (sumStack(solution) > n) {
+		return false;
+	}
+
+	for(int i{idx + 1}; i < track.size(); i++) {
+		bool solutionFound{print_solution(solution, sum, track, i, n)};
+		if (solutionFound) {
+			// printStack(solution);
+			// std::cout << "\n";
+		} else {
+			solution.pop();
+		}
+	}
+	// std::cout << "\n";
+
+	return true;	
 }
-// 1. Result is list of indices that is an actual solution
-// 2. 
-//
-// just print the sum of all combination!!!
-int get_sum(std::vector<int> tracks, int start_idx) {
-	if (start_idx == tracks.size()) {
+
+
+void printStack(std::stack<int> some_stack) {
+	if (some_stack.empty()) {
+		return;
+	}
+	int val = some_stack.top();
+	some_stack.pop();
+	printStack(some_stack);
+	std::cout << val << " ";
+	some_stack.push(val);
+}
+
+int sumStack(std::stack<int> some_stack) {
+	if (some_stack.empty()) {
 		return 0;
 	}
-	for (int i{start_idx}; i < tracks.size(); i++) {
-		std::cout << tracks.at(i) << " ";
-		int sum_below{ get_sum(tracks, i+1) };
-		std::cout << ":" << tracks.at(i) + sum_below << "\n";
-	}
-	return 0;
-}	
+	
+	int val = some_stack.top();
+	some_stack.pop();
+
+	int sum = val + sumStack(some_stack);
+	
+	some_stack.push(val);
+
+	return sum;
+}
+
