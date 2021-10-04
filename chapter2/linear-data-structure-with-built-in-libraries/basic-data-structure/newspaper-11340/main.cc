e7eef6ed84bfa4eb83a6b9620381f06c9a03329f
keenan.gebze <keenan.gebze@gmail.com>
2021-10-04 23:45:53 +0700

    newspapaer-11340 why do this work? whats wrong with the previous algo?

--- chapter2/linear-data-structure-with-built-in-libraries/basic-data-structure/newspaper-11340/main.cc
+++ chapter2/linear-data-structure-with-built-in-libraries/basic-data-structure/newspaper-11340/main.cc
@@ -2,7 +2,12 @@
 #include <vector>
 #include <string>
 #include <iomanip>
+#include <algorithm>
 
+struct char_value {
+	char c;
+	int v;
+};
 
 // need to learn about this solution, since the debug test case all pased 
 // but still get wrong answer 
@@ -19,6 +24,11 @@ int main() {
 		std::vector<int> values(256);
 		int cents{0};
 
+
+		std::vector<char_value> value2;
+		value2.reserve(100);
+
+
 		std::cin >> k;
 
 		for (int j{0}; j < k ; j++) {
@@ -28,28 +38,50 @@ int main() {
 			std::cin >> c >> value;
 
 			values[c] = value;
+			value2.push_back({c: c, v: value}); 
 		}
 
 		int lines;
 		
 		std::cin >> lines;
-		
+	
+		//for (const char_value& v : value2) {
+		//std::cout << '{' << v.c << ", " << v.v << "}" << std::endl;
+		//} 	
+
+//		for (const int i : values) {
+//			std::cout << i << ", ";
+//		}
 
 		std::string _;
 		std::getline(std::cin, _); 
 
+		// for (int j{0}; j < lines; j++ ) {
+			// std::string line;
+			// std::getline(std::cin, line);
+
+			// for (const char c : line) {
+		// 		cents += values[c];
+		// 	}
+		// }
 		for (int j{0}; j < lines; j++ ) {
 			std::string line;
 			std::getline(std::cin, line);
+			for (const char_value& c : value2) {
 
-			for (const char& c : line) {
-				cents += values[c];
+		//		std::cout << line << std::endl;
+		//		std::cout << c.c << "," << c.v << " ";
+				int count;
+				count = std::count(line.begin(), line.end(), c.c); 
+
+				cents += count * c.v;
 			}
+		//	std::cout << std::endl;
 		}
 
 		// std::cout.precision(2);
-		double dollar{ cents / (double) 100};
-		std::cout << std::setprecision(2) << std::fixed << dollar << "$" << std::endl;
+		double dollar{ cents / 100.0};
+		std::cout << std::setprecision(2) << std::fixed << dollar << '$' << std::endl;
 		
 	}
 }
